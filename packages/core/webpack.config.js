379ac394bab2096d0aef25f7fed6d55e985a5aa5
Prageeth Silva <prageethsilva@gmail.com>
2021-02-25 14:59:28 +0100

    fix externals

--- packages/core/webpack.config.js
+++ packages/core/webpack.config.js
@@ -16,8 +16,8 @@ module.exports = {
     ],
   },
   externals: {
-    "react": "React",
-    "react-dom": "ReactDOM",
+    react: path.resolve('./node_modules/react'),
+    'react-dom': path.resolve('./node_modules/react-dom')
   },
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js"],
