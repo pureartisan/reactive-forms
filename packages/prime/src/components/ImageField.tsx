210cadddfab2a68ac585ed645894754cc617011a
Prageeth Silva <prageeth@codemode.com.au>
2022-01-08 18:43:18 +1100

    add image

--- packages/prime/src/components/ImageField.tsx
+++ packages/prime/src/components/ImageField.tsx
@@ -8,12 +8,22 @@ import { PrimeImageField } from './helpers/PrimeImageField';
 import {FileData} from "../models/files";
 
 export class ImageInput extends FileInputBase<FileData> {
+    allowPreview?: boolean;
+
+    constructor(options: Partial<ImageInput>) {
+        super(options);
+
+        const defaultProps = this.defaultProps<ImageInput>();
+
+        this.allowPreview = options.allowPreview ?? defaultProps.allowPreview;
+    }
 }
 
 export const ImageField = forwardRef((props: BaseInputComponentProps<FileData, ImageInput>, ref: any) => {
     return (
         <PrimeImageField
             {...props}
+            allowPreview={props.input?.allowPreview}
             ref={ref}
         />
     );
