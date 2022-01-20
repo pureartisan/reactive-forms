992a94c75a109886b86e0fd376befbaeff22d0f6
Prageeth Silva <prageeth@codemode.com.au>
2022-01-20 23:40:24 +1100

    clean up

--- packages/core/src/hooks/form.ts
+++ packages/core/src/hooks/form.ts
@@ -6,64 +6,71 @@ import { Subscription } from '../models/observable';
 type Builder = (...args: any[]) => Form | null;
 
 type Options = {
-  valueChanges?: <T>(form: Form<T>) => void;
-  statusChanges?: <T>(form: Form<T>) => void;
-  stateChanges?: <T>(form: Form<T>) => void;
-  anythingChanges?: <T>(form: Form<T>) => void;
+    valueChanges?: <T>(form: Form<T>) => void;
+    statusChanges?: <T>(form: Form<T>) => void;
+    stateChanges?: <T>(form: Form<T>) => void;
+    anythingChanges?: <T>(form: Form<T>) => void;
 };
 
 export const useForm = <T = any>(
-  builder: Builder,
-  deps?: DependencyList,
-  opts?: Options
+    builder: Builder,
+    deps?: DependencyList,
+    opts?: Options
 ): {
     form: Form<T> | undefined,
     reset: () => void;
 } => {
-  const subscriptions = useRef<Subscription<any>[]>([]);
-  const [form, setForm] = useState<Form<T> | undefined>(undefined);
+    const subscriptions = useRef<Subscription<any>[]>([]);
+    const [form, setForm] = useState<Form<T> | undefined>(undefined);
 
-  // reset any previous subscriptions
-  const unsubscribeAllSubscriptions = () => subscriptions.current?.forEach(s => s.unsubscribe());
+    // reset any previous subscriptions
+    const unsubscribeAllSubscriptions = () => subscriptions.current?.forEach(s => s.unsubscribe());
 
-  const reset = () => {
-      unsubscribeAllSubscriptions();
-      const f = builder();
-      if (f) {
-          subscriptions.current = [];
+    const buildForm = () => {
+        unsubscribeAllSubscriptions();
+        const f = builder();
 
-          if (opts?.valueChanges) {
-              subscriptions.current.push(f.valueChanges?.subscribe(() => {
-                  opts?.valueChanges && opts.valueChanges(f);
-              }));
-          }
-          if (opts?.statusChanges) {
-              subscriptions.current.push(f.statusChanges?.subscribe(() => {
-                  opts?.statusChanges && opts.statusChanges(f);
-              }));
-          }
-          if (opts?.stateChanges) {
-              subscriptions.current.push(f.stateChanges?.subscribe(() => {
-                  opts?.stateChanges && opts.stateChanges(f);
-              }));
-          }
-          if (opts?.anythingChanges) {
-              subscriptions.current.push(f.anythingChanges?.subscribe(() => {
-                  opts?.anythingChanges && opts.anythingChanges(f);
-              }));
-          }
+        if (f) {
+            subscriptions.current = [];
 
-          setForm(f);
-      }
-  }
+            if (opts?.valueChanges) {
+                subscriptions.current.push(f.valueChanges?.subscribe(() => {
+                    opts?.valueChanges && opts.valueChanges(f);
+                }));
+            }
+            if (opts?.statusChanges) {
+                subscriptions.current.push(f.statusChanges?.subscribe(() => {
+                    opts?.statusChanges && opts.statusChanges(f);
+                }));
+            }
+            if (opts?.stateChanges) {
+                subscriptions.current.push(f.stateChanges?.subscribe(() => {
+                    opts?.stateChanges && opts.stateChanges(f);
+                }));
+            }
+            if (opts?.anythingChanges) {
+                subscriptions.current.push(f.anythingChanges?.subscribe(() => {
+                    opts?.anythingChanges && opts.anythingChanges(f);
+                }));
+            }
 
-  useEffect(() => {
-      reset();
-      return unsubscribeAllSubscriptions;
-  }, deps);
+            setForm(f);
+        }
 
-  return {
-      form,
-      reset
-  };
+        return f;
+    }
+
+    const reset = () => {
+        buildForm();
+    }
+
+    useEffect(() => {
+        buildForm();
+        return unsubscribeAllSubscriptions;
+    }, deps);
+
+    return {
+        form,
+        reset
+    };
 };
