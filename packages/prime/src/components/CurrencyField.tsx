94aeb0bb252f1a4ceac5d452ab72f8305c0d9d0f
Prageeth Silva <prageeth@codemode.com.au>
2022-09-17 02:53:29 +1000

    Add Input number from prime for currency

--- packages/prime/src/components/CurrencyField.tsx
+++ packages/prime/src/components/CurrencyField.tsx
@@ -4,41 +4,57 @@ import { BaseInputComponentProps } from '@reactiveforms/core';
 
 import { TextInputBase } from '../models/input-base';
 
-import { PrimeTextField } from './helpers/PrimeTextField';
+import { PrimeNumberField } from './helpers/PrimeNumberField';
 
-export class CurrencyInput extends TextInputBase<string> {
-
-    currencyIcon?: any;
-    alignIcon?: 'left' | 'right';
+export class CurrencyInput extends TextInputBase<number> {
+    currency?: string;
+    currencyDisplay?: string;
+    useGrouping?: boolean;
+    min?: number;
+    max?: number;
+    minFractionDigits?: number;
+    maxFractionDigits?: number;
+    mode?: string;
+    locale?: string;
+    prefix?: string;
+    suffix?: string;
 
     constructor(options: Partial<CurrencyInput>) {
         super(options);
 
         const defaultProps = this.defaultProps<CurrencyInput>();
 
-        this.currencyIcon = options.currencyIcon ?? defaultProps.currencyIcon;
-        this.alignIcon = options.alignIcon ?? defaultProps.alignIcon ?? 'left';
+        this.currency = options.currency ?? defaultProps.currency;
+        this.currencyDisplay = options.currencyDisplay ?? defaultProps.currencyDisplay;
+        this.useGrouping = options.useGrouping ?? defaultProps.useGrouping;
+        this.min = options.min ?? defaultProps.min;
+        this.max = options.max ?? defaultProps.max;
+        this.minFractionDigits = options.minFractionDigits ?? defaultProps.minFractionDigits;
+        this.maxFractionDigits = options.maxFractionDigits ?? defaultProps.maxFractionDigits;
+        this.mode = options.mode ?? defaultProps.mode;
+        this.locale = options.locale ?? defaultProps.locale;
+        this.prefix = options.prefix ?? defaultProps.prefix;
+        this.suffix = options.suffix ?? defaultProps.suffix;
     }
 }
 
-export const CurrencyField = forwardRef((props: BaseInputComponentProps<string, CurrencyInput>, ref: any) => {
-
-    const showIcon = Boolean(props.input?.currencyIcon);
-
+export const CurrencyField = forwardRef((props: BaseInputComponentProps<number, CurrencyInput>, ref: any) => {
     return (
-        <PrimeTextField
+        <PrimeNumberField
             {...props}
             ref={ref}
-            type="text"
             inputType="CurrencyField"
-            leftIcon={showIcon && props.input?.alignIcon === 'left' && (
-                props.input?.currencyIcon
-            )}
-            rightIcon={showIcon && props.input?.alignIcon === 'right' && (
-                props.input?.currencyIcon
-            )}
-            leftIconClassName="currency-symbol"
-            rightIconClassName="currency-symbol"
+            currency={props.input?.currency}
+            currencyDisplay={props.input?.currencyDisplay}
+            useGrouping={props.input?.useGrouping}
+            min={props.input?.min}
+            max={props.input?.max}
+            minFractionDigits={props.input?.minFractionDigits}
+            maxFractionDigits={props.input?.maxFractionDigits}
+            mode={props.input?.mode}
+            locale={props.input?.locale}
+            prefix={props.input?.prefix}
+            suffix={props.input?.suffix}
         />
     );
 });
---
+++ packages/prime/src/components/helpers/PrimeNumberField.tsx
@@ -0,0 +1,94 @@
+import React, { forwardRef } from 'react';
+import clsx from 'clsx';
+
+import {InputNumber, InputNumberChangeParams} from "primereact/inputnumber";
+
+import { BaseInputComponentProps, InputBase, BaseControl, getFirstErrorMsg } from '@reactiveforms/core';
+
+import { TextInputBase } from '../../models/input-base';
+import { getHelpText } from '../../utils/helpers';
+
+interface PrimeNumberFieldProps<V extends number, I extends InputBase<V> = any, C extends BaseControl<V> = BaseControl<V>> extends BaseInputComponentProps<V, I, C> {
+    inputType?: string;
+    currency?: string;
+    currencyDisplay?: string;
+    useGrouping?: boolean;
+    min?: number;
+    max?: number;
+    minFractionDigits?: number;
+    maxFractionDigits?: number;
+    mode?: string;
+    locale?: string;
+    prefix?: string;
+    suffix?: string;
+}
+
+export const PrimeNumberField = forwardRef(<V extends number, I extends TextInputBase<V>>(props: PrimeNumberFieldProps<V, I>, ref: any) => {
+    const firstError = getFirstErrorMsg(props);
+
+    const handleChange = (event: InputNumberChangeParams) => {
+        const value = event?.value as unknown as V;
+        props.control?.setValue(value, { emitEvent: false });
+        if (value === props.input?.value) {
+            props.control?.markAsPristine({ emitEvent: false });
+        } else {
+            props.control?.markAsDirty({ emitEvent: false });
+        }
+        props.control?.emitEvents();
+    };
+
+    const helpText = getHelpText(props.input, props.control, firstError);
+
+    return (
+        <div
+            className={clsx(
+                `rf-field rf-field-${props.inputType} rf-field-name-${props.input?.name} field`,
+                props.input?.className,
+                {
+                    'col-12': !props.input?.className,
+                    'input-empty': !props.control?.value
+                }
+            )}
+        >
+            {props.input?.label && (
+                <label htmlFor={props.input?.id} className={props.input?.labelClassName}>
+                    {props.input.label as any}
+                </label>
+            )}
+            <InputNumber
+                id={props.input?.id}
+                placeholder={props.input?.placeholder}
+                readOnly={props.input?.readonly}
+                disabled={props.control?.disabled}
+                value={props.control?.value}
+                onChange={handleChange}
+                currency={props.currency}
+                currencyDisplay={props.currencyDisplay}
+                useGrouping={props.useGrouping}
+                min={props.min}
+                max={props.max}
+                minFractionDigits={props.minFractionDigits}
+                maxFractionDigits={props.maxFractionDigits}
+                mode={props.mode}
+                locale={props.locale}
+                prefix={props.prefix}
+                suffix={props.suffix}
+                className={clsx('w-full', props.input?.inputClassName)}
+                ref={ref}
+            />
+            {helpText && (
+                <small
+                    className={clsx(
+                        "rf-helper-text block",
+                        { 'p-error': firstError },
+                        props.input?.hintClassName
+                    )}
+                >
+                    {helpText}
+                </small>
+            )}
+        </div>
+    );
+});
+
+PrimeNumberField.displayName = 'PrimeNumberField';
