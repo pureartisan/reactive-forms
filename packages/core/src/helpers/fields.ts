6bcdbf555e408a35e8264f2981a74a6c3029d534
Prageeth Silva <prageeth@codemode.com.au>
2022-11-25 15:52:35 +1100

    allow nullables

--- packages/core/src/helpers/fields.ts
+++ packages/core/src/helpers/fields.ts
@@ -1,4 +1,4 @@
 import {InputBase} from "../models/inputs";
 
-export const fieldCssCls = <T = any>(input: InputBase<T>, type?: string) =>
+export const fieldCssCls = <T = any>(input?: InputBase<T>|null, type?: string) =>
         `rf-field ${type ? 'rf-field-'+type : ''} ${input?.name ? 'rf-field-name-'+input?.name : ''}`;
