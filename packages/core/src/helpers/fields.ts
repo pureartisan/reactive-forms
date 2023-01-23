ef9a27d2df9927e3db106a81df0e81aa0411feea
Prageeth Silva <prageeth@codemode.com.au>
2023-01-24 04:00:30 +1100

    include required class in core

--- packages/core/src/helpers/fields.ts
+++ packages/core/src/helpers/fields.ts
@@ -1,4 +1,12 @@
 import {InputBase} from "../models/inputs";
+import {Validators} from "../validation/validators";
 
 export const fieldCssCls = <T = any>(input?: InputBase<T>|null, type?: string) =>
-        `rf-field ${type ? 'rf-field-'+type : ''} ${input?.name ? 'rf-field-name-'+input?.name : ''}`;
+        `rf-field ${type ? 'rf-field-'+type : ''} ${input?.name ? 'rf-field-name-'+input?.name : ''} ${hasRequiredValidator(input) ? 'has-required' : ''}`;
+
+export const hasRequiredValidator = <T>(input?: InputBase<T>|null): boolean => {
+    if (Array.isArray(input?.validators)) {
+        return input?.validators.some((validator: any) => validator === Validators.required) ?? false;
+    }
+    return input?.validators === Validators.required;
+};
